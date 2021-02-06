dee76e3c5b899a6b1544874f493439284e457f13
purpl3f0xsecur1ty <42351007+purpl3f0xsecur1ty@users.noreply.github.com>
2021-02-06 13:55:17 -0500

    Add files via upload

---
+++ B64_encode_cmd.ps1
@@ -0,0 +1,5 @@
+﻿$text = Read-Host -Prompt 'Command to base64 encode '
+$bytes = [System.Text.Encoding]::Unicode.GetBytes($text)
+$EncodedText = [Convert]::ToBase64String($bytes)
+Write-Host "Encoded string: "
+Write-Host $EncodedText
