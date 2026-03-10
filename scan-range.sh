b03603278631c0ca3dcdfe9b3d44911d441f216e
Purpl3F0x <42351007+purpl3f0xsecur1ty@users.noreply.github.com>
2026-03-10 10:35:26 -0400

    Update scan-range.sh

--- scan-range.sh
+++ scan-range.sh
@@ -1,14 +1,14 @@
 # For this script, make sure targets are in CIDR notation:
 # 192.168.0.0/24
+# Make sure to run as root using "sudo"
 
 for cidr in `cat targets.txt`; do
+  dir=$(pwd)
   echo ${cidr}
   netblock=`echo ${cidr} | cut -f 1 -d '/'`
   echo ${netblock}
-  # CHANGE ME!!
-  mkdir -p /home/<YOUR USER HERE>/${netblock}
-  # CHANGE ME!!
-  cd /home/<YOUR USER HERE>/${netblock}
+  mkdir -p $dir/${netblock}
+  cd $dir/${netblock}
   masscan --rate=5000 -p1- -oG ${netblock}-masscan.grep ${cidr}
   cat ${netblock}-masscan.grep | grep "Host:" | grep Timestamp | cut -f 3 -d " " | sort -u > living-hosts
   cat ${netblock}-masscan.grep | grep "Host:" | grep Timestamp | cut -f 5 -d " " | cut -f 1 -d '/' | sort -u > portlist
@@ -16,6 +16,5 @@ for cidr in `cat targets.txt`; do
           echo -n ${i},
   done > portlist-commas
   nmap -A -sS -sV -p $(cat portlist-commas | sed -e 's/,$//') -iL living-hosts -oA living-hosts -v -T3
-  # CHANGE ME!!
-  cd /home/<YOUR USER HERE>
+  cd $dir
 done
